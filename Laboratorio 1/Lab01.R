2411e7351445da843c777489b961f0c087dae7cd
invitado <invitado@a13.ac.labf.usb.ve>
2019-02-15 12:19:38 -0500

    Se añade pregunta 2 completa y boxplot en .pdf de pregunta 2

--- Laboratorio 1/Lab01.R
+++ Laboratorio 1/Lab01.R
@@ -8,20 +8,35 @@ consumo = as.vector(mercado$Consumo)
 
 # PREGUNTA 2
 
+# Se filtran los datos según lo requerido (carnico y cambur como fruta)
 F_consumo_carne = consumo [carnico == "Carne" & genero == "F" & fruta == "Cambur"]
+# Vemos como se comportan los datos a nivel numerico
 summary(F_consumo_carne)
 
+# Se filtran los datos según lo requerido (carnico y cambur como fruta)
 F_consumo_cerdo = consumo [carnico == "Cerdo" & genero == "F" & fruta == "Cambur"]
+# Vemos como se comportan los datos a nivel numerico
 summary(F_consumo_cerdo)
 
+# Se filtran los datos según lo requerido (carnico y cambur como fruta)
 F_consumo_pescado = consumo [carnico == "Pescado" & genero == "F" & fruta == "Cambur"]
+# Vemos como se comportan los datos a nivel numerico
 summary(F_consumo_pescado)
 
+# Se filtran los datos según lo requerido (carnico y cambur como fruta)
 F_consumo_pollo = consumo [carnico == "Pollo" & genero == "F" & fruta == "Cambur"]
+# Vemos como se comportan los datos a nivel numerico
 summary(F_consumo_pollo)
 
+# Hacemos un boxplot triple para ver mejor como se comportan los datos
 boxplot(F_consumo_carne, F_consumo_cerdo, F_consumo_pollo,
-        names = c("Pollo", "Cerdo", "Carne"))
+        
+        main = "Dinero gastado por mujeres que compraron cambur según carnico",
+        ylab = "Consumo",
+        xlab = "Carnico",
+        
+        names = c("Carne", "Cerdo", "Pollo"),
+        col = c("red", "blue", "green"))
 
 # PREGUNTA 3
 
