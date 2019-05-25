f1e6d7dd8ebf9b396c91652fc88ba448ca55fc6c
Antonella <antonellalrc@gmail.com>
2019-05-25 09:10:15 -0400

    agrega ejercicio 3

--- Laboratorio 3/Lab03.R
+++ Laboratorio 3/Lab03.R
@@ -31,3 +31,53 @@ fi = c(294, 276, 238, 192)
 
 prop.test(fi, c(1000,1000,1000,1000), p)
 
+### PREGUNTA 3
+
+# Como tenemos datos no agrupados, vamos a dividir las clases
+# para obtener la frecuencia
+n = 50
+alpha = 0.05
+r = 2
+
+xi = c(32, 23, 64, 31, 74, 44, 61, 33, 66, 73,
+       27, 65, 40, 54, 23, 43, 58, 87, 58, 62,
+       68, 89, 93, 24, 73, 42, 33, 63, 36, 48,
+       77, 75, 37, 59, 70, 61, 43, 68, 54, 29,
+       48, 81, 57, 97, 35, 58, 56, 58, 57, 45)
+
+hist(xi, main = "Histograma de Edades", ylab = "Frecuencia", xlab = "Edades", col = "blue")
+
+# Ahora, agrupando los datos
+k = 4
+fi = c(12,19,14,5)
+mi= c(30, 50, 70, 90)
+lu = c(40, 60, 80, 100)  # limite superior de cada clase
+ll = c(20, 40, 60, 80)   # limite inferior de cada clase
+
+
+# Hacemos qq-plot
+qqnorm(fi)
+qqline(fi)
+
+# Determinamos el promedio
+prom = sum(fi*mi)/n
+      
+# Ahora la varianza
+prom_n = rep(prom,n)
+
+varianza = sum((xi-prom_n)^2 )/(n-1)
+
+# Desviacion estandar
+sd = sqrt(varianza)
+
+# Una vez que tenemos varianza y promedio podemos calcular la probabilidad pi
+pi = pnorm(lu, prom, sd) - pnorm (ll, prom, sd)
+
+# Calculamos nuestro estadistico Chi-cuadrado
+chi2_est = sum((fi-n*pi)^2/(n*pi))
+
+# Determinamos la region de rechazo
+chi2_rr = qchisq(1-alpha, k-1-r)
+
+# Como no se nos da el nivel de significancia alpha, calculamos el p-valor
+p-valor = 1 - pchisq(chi2_obs, n-1-2)
