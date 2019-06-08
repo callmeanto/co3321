696bdd3bbb3eae0339f794d5083e5444eb968147
Arturo Yepez <arturyepez10@gmail.com>
2019-06-07 22:20:19 -0400

    Lab 4 con archivo (sin enunciado)

---
+++ Laboratorio 4/Lab04.R
@@ -0,0 +1,119 @@
+# Laboratorio 4
+# Alumnos: Arturo Yepez 15-11551, Antonella Requena 15-11196
+
+# CARGAR LOS DATOS
+# Cargamos el estado
+data(state)
+
+# Convertimos el estado en un data frame
+edo.77 = as.data.frame(state.x77)
+
+# Cambiamos algunos nombres para evitar problemas
+names(edo.77)[4] = "Life.Exp"
+names(edo.77)[6] = "Hs.Grad"
+
+# Se extraen las secciones de interes del data frame
+
+
+### PREGUNTA 1
+
+# Resumenes de los datos (Cuartiles, Medianas, Mínimo, Máximo y Media)
+summary( edo.77[1] )
+
+summary( edo.77[2] )
+
+summary( edo.77[3] )
+
+summary( edo.77[4] )
+
+summary( edo.77[5] )
+
+summary( edo.77[6] )
+
+summary( edo.77[7] )
+
+summary( edo.77[8] )
+
+# Desviación Estandar de los datos
+sd( edo.77[1] )
+
+sd( edo.77[2] )
+
+sd( edo.77[3] )
+
+sd( edo.77[4] )
+
+sd( edo.77[5] )
+
+sd( edo.77[6] )
+
+sd( edo.77[7] )
+
+sd( edo.77[8] )
+
+# Diagramas de Caja de los 8 datos
+boxplot(edo.77[1], 
+        
+        main = "Populación",
+        ylab = "No. de Habitantes",
+        
+        col = "white")
+
+boxplot(edo.77[2], 
+        
+        main = "Ingresos",
+        ylab = "Renta per Capita",
+        
+        col = "green")
+
+boxplot(edo.77[3], 
+        
+        main = "Analfabetismo",
+        ylab = "Porcentaje de la Población",
+        
+        col = "orange")
+
+boxplot(edo.77[4], 
+        
+        main = "Esperanza de Vida",
+        ylab = "Años",
+        
+        col = "light blue")
+
+boxplot(edo.77[5], 
+        
+        main = "Asesinato y Homicidio No Negligente",
+        ylab = "No. de Personas",
+        
+        col = "red")
+
+boxplot(edo.77[6], 
+        
+        main = "Graduados de Escuela Secundaria",
+        ylab = "Porcentaje de la Población",
+        
+        col = "beige")
+
+boxplot(edo.77[7], 
+        
+        main = "Temperatura por Debajo del Punto de Congelación",
+        ylab = "No. de Días",
+        
+        col = "dark blue")
+
+boxplot(edo.77[8], 
+        
+        main = "Area de Tierra",
+        ylab = "Millas Cuadradas",
+        
+        col = "brown")
+
+### PREGUNTA 2
+
+graf_matriz_cor = pairs( edo.77, labels = c("Populación", "Ingresos", "Analfabetismo", "Esperanzas de Vida",
+                          "Asesinato", "Graduaos de High School", "Temperatura", 
+                          "Area de Tierra") )
+
+matriz_cor = cor(edo.77)
+
+### PREGUNTA 3
